f99e914640cec506ab9421e9ff893bf0349cb804
Jeremy Brown <jsb@jetcom.org>
2019-10-01 16:20:48 -0400

    Restructured code to take in the arguments instead of hardcoded paths.

--- mkt_reader_writer.py
+++ mkt_reader_writer.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 from configobj import ConfigObj, Section
+import os
 
-def load_questions_file(f_name):
-   obj = ConfigObj(f_name, interpolation=True)   
-   f_name = f_name.split("/")
-   f_name = f_name[len(f_name)-1]
-   return obj, load_questions(obj, f_name)
+root_path = "../questions/"
+questionPool_path = "/questionPool/"
+
+
+def get_courses():
+    courses = [name for name in os.listdir(root_path)
+            if os.path.isdir(os.path.join(root_path, name))]
+    courses.sort()
+    return courses
+
+def get_questions_files(course):
+    files = os.listdir(root_path+course+questionPool_path)
+    files.sort()
+    return str(files).strip('[]')
+
+
+def load_questions_file(course, question_file):
+    f_name = root_path + course + questionPool_path + question_file
+    obj = ConfigObj(f_name, interpolation=True)
+    f_name = f_name.split("/")
+    f_name = f_name[len(f_name)-1]
+    return obj, load_questions(obj, f_name)
 
 def load_questions(obj, parent):
     qList = {}    
@@ -17,7 +35,8 @@ def load_questions(obj, parent):
             qList.update(load_questions(item, parent + "/" + c))
     return qList
     
-def get_question(questions, key):
+def get_question(questions, file, title):
+    key = file+"/"+title
     return questions[key]
     
 def update_question(question, key, value):
@@ -27,8 +46,10 @@ def save_changes(f_name, obj):
     obj.write(open(f_name, "wb"))
 
 if __name__ == "__main__":
-    obj, questions = load_questions_file("csci320-2191/questionPool/chapter1.txt")
-    question = get_question(questions, 'chapter1.txt/File System')
+    if not os._exists("../questions/"):
+        root_path = "./questions/"
+    obj, questions = load_questions_file("csci320-2191", "chapter1.txt")
+    question = get_question(questions, 'chapter1.txt', 'File System')
     update_question( question, 'points', 100)
     save_changes('test.txt', obj)
 
--- web/index.py
+++ web/index.py
@@ -4,41 +4,30 @@ import os
 import json
 
 
-def get_immediate_subdirectories(a_dir):
-    return [name for name in os.listdir(a_dir)
-            if os.path.isdir(os.path.join(a_dir, name))]
-
-
-courses = get_immediate_subdirectories('../questions/')
-courses.sort()
-
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', courses=courses)
+    return render_template('home.html', courses=mkt_reader_writer.get_courses())
 
 @app.route('/editor')
 def editor():
-
-    return render_template('editor.html', courses=courses)
+    return render_template('editor.html', courses=mkt_reader_writer.get_courses())
 
 
 @app.route('/changeCourse', methods=['POST'])
 def changeCourse():
     course = request.form['selectedCourse']
-    files = os.listdir("../questions/"+course+'/questionPool')
-    files.sort()
-    return str(files).strip('[]')
+    return mkt_reader_writer.get_questions_files(course)
     
 
 @app.route('/getQuestions', methods=['POST'])
 def getQuestions():
     course = request.form['course'].strip()
     fileName = request.form['fileName'].strip()
-    obj, questions = mkt_reader_writer.load_questions_file("../questions/"+course+'/questionPool/'+fileName)
+    obj, questions = mkt_reader_writer.load_questions_file(course, fileName)
     return json.dumps(obj)
 
 @app.route('/addQuestion', methods=['POST'])
